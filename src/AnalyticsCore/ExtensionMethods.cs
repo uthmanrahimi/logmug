b9e45eed26ac671705e0dfc30c7a40d171adc95f
Uthman <uthmanrahimi@gmail.com>
2021-03-06 20:08:59 +0330

    added a middleware

---
+++ src/AnalyticsCore/ExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace AnalyticsCore
+{
+    public static class ExtensionMethods
+    {
+        public static IApplicationBuilder UseAnalyticsCore(this IApplicationBuilder builder, IDataStore storeProvider)
+        {
+            //register middleware here
+            return builder;
+        }
+    }
+}
---
+++ src/AnalyticsCore/RequestLoggerMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalyticsCore
+{
+    public class RequestLoggerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestLoggerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _next(context);
+
+
+        }
+    }
+}
